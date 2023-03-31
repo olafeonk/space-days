6ea4b520e102c52e41b277913e73c22892fd90b8
Evgeny Nikolaev <eannikolaev@gmail.com>
2023-03-31 13:45:00 +0500

    шрифт + фото

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -15,34 +15,34 @@ function Carousel() {
       <input type="radio" name="slider" id="item-10" />
       <div className="cards">
         <label className="card" htmlFor="item-1" id="photo-1">
-          <img src="./image/2022/1.png" alt="дни космоса 2022" />
+          <img src="./image/2022/1.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-2" id="photo-2">
-          <img src="./image/2022/2.png" alt="дни космоса 2022" />
+          <img src="./image/2022/2.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-3" id="photo-3">
-          <img src="./image/2022/3.png" alt="дни космоса 2022" />
+          <img src="./image/2022/3.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-4" id="photo-4">
-          <img src="./image/2022/4.png" alt="дни космоса 2022" />
+          <img src="./image/2022/4.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-5" id="photo-5">
-          <img src="./image/2022/5.png" alt="дни космоса 2022" />
+          <img src="./image/2022/5.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-6" id="photo-6">
-          <img src="./image/2022/6.png" alt="дни космоса 2022" />
+          <img src="./image/2022/6.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-7" id="photo-7">
-          <img src="./image/2022/7.png" alt="дни космоса 2022" />
+          <img src="./image/2022/7.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-8" id="photo-8">
-          <img src="./image/2022/8.png" alt="дни космоса 2022" />
+          <img src="./image/2022/8.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-9" id="photo-9">
-          <img src="./image/2022/9.png" alt="дни космоса 2022" />
+          <img src="./image/2022/9.jpeg" alt="дни космоса 2022" />
         </label>
         <label className="card" htmlFor="item-10" id="photo-10">
-          <img src="./image/2022/10.png" alt="дни космоса 2022" />
+          <img src="./image/2022/10.jpeg" alt="дни космоса 2022" />
         </label>
       </div>
     </Container>
