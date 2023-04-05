92c63e8b171f19b758215ec53bcd5155c1cc566c
Evgeny Nikolaev <eannikolaev@gmail.com>
2023-04-05 15:33:58 +0500

    новые партнеры

--- frontend/src/components/PartnerCarousel.jsx
+++ frontend/src/components/PartnerCarousel.jsx
@@ -7,113 +7,161 @@ function PartnerCarousel() {
       <a href="https://zsfond.ru">
         <Image
           src="./image/partners/zolotoesechenie.png"
-          alt="zolotoe sechenie"
+          alt='Фонд "Золотое сечение"'
         ></Image>
       </a>
       <a href="https://targem.ru">
         <Image
           src="./image/partners/targemgames.png"
-          alt="targem games"
+          alt="Targem Games "
         ></Image>
       </a>
       <a href="https://sndc.ru">
-        <Image src="./image/partners/soundcity.png" alt="soundcity"></Image>
+        <Image
+          src="./image/partners/soundcity.png"
+          alt='Школа музыки "Sound city"'
+        ></Image>
       </a>
       <a href="https://fiit-urfu.ru/">
-        <Image src="./image/partners/fiit.png" alt="fiit"></Image>
+        <Image
+          src="./image/partners/fiit.png"
+          alt="IT-направление ФИИТ в УрФУ"
+        ></Image>
       </a>
       <a href="https://smartschoolekb.ru">
         <Image
           src="./image/partners/smartschool.png"
-          alt="smart school"
+          alt="Современная частная школа СМАРТ"
         ></Image>
       </a>
       <a href="https://s-coolbus.ru">
-        <Image src="./image/partners/schoolbus.png" alt="school bus"></Image>
+        <Image
+          src="./image/partners/schoolbus.png"
+          alt='ЯЗЫКОВОЙ ЦЕНТР "School bus"'
+        ></Image>
       </a>
       <a href="https://mkugmk.ru/ru/about-complex/exhibition-centers/planetariy/">
-        <Image src="./image/partners/planetariy.png" alt="planetariy"></Image>
+        <Image
+          src="./image/partners/planetariy.png"
+          alt="Планетарий Екатеринбург"
+        ></Image>
       </a>
       <a href="http://gifted.ru">
-        <Image src="./image/partners/odarennost.png" alt="odarennost"></Image>
+        <Image
+          src="./image/partners/odarennost.png"
+          alt='Дворец творчества "Одарённость и технологии"'
+        ></Image>
       </a>
       <a href="https://www.npoa.ru">
-        <Image src="./image/partners/npo.png" alt="NPO"></Image>
+        <Image src="./image/partners/npo.png" alt="НПО автоматики "></Image>
       </a>
       <a href="https://ugvim.ru">
-        <Image src="./image/partners/muzeivdv.png" alt="muzei VDV"></Image>
+        <Image
+          src="./image/partners/muzeivdv.png"
+          alt="Уральский государственный военно-исторический музей"
+        ></Image>
       </a>
       <a href="http://молодежь.екатеринбург.рф">
         <Image
           src="./image/partners/molodeznayapolicy.png"
-          alt="molodeznaya policy"
+          alt="Управление молодёжной политики администрации города Екатеринбурга"
         ></Image>
       </a>
       <a href="https://lotsman.ru">
-        <Image src="./image/partners/locman.png" alt="locman"></Image>
+        <Image
+          src="./image/partners/locman.png"
+          alt='Детская общественная организация "Лоцман"'
+        ></Image>
       </a>
       <a href="https://m-i-e.ru/l52">
-        <Image src="./image/partners/l52.png" alt="l52"></Image>
+        <Image
+          src="./image/partners/l52.png"
+          alt="Л52 - Музей истории Екатеринбурга"
+        ></Image>
       </a>
       <a href="https://kantrskrip.ru">
-        <Image src="./image/partners/kantrscrip.png" alt="kantr scrip"></Image>
+        <Image
+          src="./image/partners/kantrscrip.png"
+          alt="Школа астрономии"
+        ></Image>
       </a>
       <a href="https://igronaftik.ru">
-        <Image src="./image/partners/igronaphtic.png" alt="igronaphtic"></Image>
+        <Image
+          src="./image/partners/igronaphtic.png"
+          alt='Мастерская игр "Игронафтик"'
+        ></Image>
       </a>
       <a href="https://myatom.ru">
-        <Image src="./image/partners/icae.png" alt="ICAE"></Image>
+        <Image
+          src="./image/partners/icae.png"
+          alt="Информационные центры по атомной энергии"
+        ></Image>
       </a>
       <a href="https://ит-школа.рф/index.html">
         <Image
           src="./image/partners/homecomputer.png"
-          alt="home computer"
+          alt="Школа программирования «Домашний компьютер»"
         ></Image>
       </a>
       <a href="https://vnauku.ru/">
         <Image
           src="./image/partners/hochuvnauku.png"
-          alt="hochu v nauku"
+          alt="ФОНД ПОДДЕРЖКИ ИНИЦИАТИВ В СФЕРЕ ДОПОЛНИТЕЛЬНОГО ОБРАЗОВАНИЯ"
         ></Image>
       </a>
       <a href="https://englishdrive.ru">
         <Image
           src="./image/partners/englishdrive.png"
-          alt="english drive"
+          alt='Языковой лагерь "English drive"'
         ></Image>
       </a>
       <a href="https://eklat.jimdofree.com">
-        <Image src="./image/partners/ekbclub.png" alt="ekb club"></Image>
+        <Image
+          src="./image/partners/ekbclub.png"
+          alt="Екатеринбургкий клуб любителей асрономии и телескопостроения"
+        ></Image>
       </a>
       <a href="https://vk.com/cosmoport.club">
-        <Image src="./image/partners/cosmoport.png" alt="cosmoport"></Image>
+        <Image src="./image/partners/cosmoport.png" alt="Космопорт"></Image>
       </a>
       <a href="http://айтикуб.рф">
         <Image
           src="./image/partners/centeritcube.png"
-          alt="center IT-cube"
+          alt="ЦЕНТРЫ ЦИФРОВОГО ОБРАЗОВАНИЯ ДЕТЕЙ «IT-КУБ»"
         ></Image>
       </a>
       <a href="http://book.uraic.ru">
         <Image
           src="./image/partners/bibliotekabelinskogo.png"
-          alt="biblioteka belinskogo"
+          alt="Библиотека Белинского"
         ></Image>
       </a>
       <a href="https://events.bgekb.ru">
         <Image
           src="./image/partners/bibliotechniycenter.png"
-          alt="bibliotechniy center"
+          alt='Библиотечный центр "Екатеринбург"'
         ></Image>
       </a>
       <a href="https://екатеринбург.рф">
         <Image
           src="./image/partners/administration.png"
-          alt="administration"
+          alt="Администрация Екатеринбурга"
         ></Image>
       </a>
       <a href="https://www.4glaza.ru/shops/ekaterinburg/">
-        <Image src="./image/partners/4glaza.png" alt="4 glaza"></Image>
+        <Image
+          src="./image/partners/4glaza.png"
+          alt='Магазин "4 глаза"'
+        ></Image>
+      </a>
+      <a href="https://baby-club.ru/clubs/ekaterinburg/academic/?utm_source=yandex&utm_medium=map&utm_campaign=ad_mart">
+        <Image src="./image/partners/babyklub.png" alt="Бэби клуб"></Image>
+      </a>
+      <a href="https://vk.com/pripev_akadem">
+        <Image
+          src="./image/partners/pripev.png"
+          alt='Школа вокала "Припев"'
+        ></Image>
       </a>
     </Marquee>
   );
