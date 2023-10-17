24c0a56b82fe26b0d7c8b2033e64809f8ec122d7
Evgeny Nikolaev <eannikolaev@gmail.com>
2023-10-17 11:32:46 +0500

    update front new scripts

---
+++ frontend/update_frontend_production_aws.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#variables in env file
+if [ -f ".env.production" ]; then
+    export $(grep -v '^#' .env.production | xargs)
+fi
+
+if ! [ "${SPACE_DAYS_APP_BUCKET}" ]; then
+    echo "SPACE_DAYS_APP_BUCKET is not set!"
+    exit 1
+fi
+
+cd ./build
+aws s3 sync . s3://${SPACE_DAYS_APP_BUCKET}
+aws s3 --recursive modify --acl-public --add-header='content-type':'text/javascript' --exclude '' --include '.js' s3://${SPACE_DAYS_APP_BUCKET}
+aws s3 --recursive modify --acl-public --add-header='content-type':'text/css' --exclude '' --include '.css' s3://${SPACE_DAYS_APP_BUCKET}
+cd ..
